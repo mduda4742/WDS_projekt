218223d2f228865436ffdcc8803718756994e04e
GoofyGarry420 <radekmijal@gmail.com>
2026-04-21 21:54:01 +0200

    added Doxygen documentation basic layout

--- inc/window.hpp
+++ inc/window.hpp
@@ -10,25 +10,65 @@
 #include <QGridLayout>
 #include <QTabWidget>
 
+/**
+ * @class Window
+ * @brief Main application window class that inherits from QMainWindow. 
+ * This class sets up the GUI with multiple tabs for different functionalities 
+ * such as Home, SLAM, and IMU data display. It also includes slots to update 
+ * the GUI based on signals emitted from the ROS node.
+ */
 class Window : public QMainWindow {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for the Window class. Initializes the main window, sets up the 
+     * tab widget, and creates the different pages for Home, SLAM, and IMU.
+     */
     Window(QWidget *parent = nullptr);
+    /**
+     * @brief Destructor for the Window class. Currently defaulted, but can be used to clean up any resources if needed in the future.
+     */
     virtual ~Window() = default;
 
 public slots:
+    /**
+     * @brief Slot to update the test data displayed in the GUI.
+     * @param msg The new test data message to display.
+     */
     void updateTestData(const QString &msg);
 
 private:
+    /**
+     * @brief Pointer to the QTabWidget that holds the different pages of the GUI. 
+     * This allows for easy access and manipulation of the tabs and their contents.
+     */
     QTabWidget *tabWidget;
 
+    /**
+     * @brief Helper function to create the Home page of the GUI. 
+     */
     QWidget *createHomePage();
+    /**
+     * @brief Helper function to create the SLAM page of the GUI. 
+     */
     QWidget *createSlamPage();
+    /**
+     * @brief Helper function to create the IMU page of the GUI. 
+     */
     QWidget *createImuPage();
 
+    /**
+     * @brief Pointer to the QLabel used as a placeholder for the SLAM visualization. 
+     */
     QLabel *mapPlaceholder;
+    /**
+     * @brief Pointers to the QLabel widgets used to display accelerometer data on the IMU page.
+     */
     QLabel *accelLabel;
+    /**
+     * @brief Pointers to the QLabel widgets used to display gyroscope data on the IMU page.
+     */
     QLabel *gyroLabel;
 };
 
